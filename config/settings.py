b0219f89fcfae6ef616f4be4e5d0846be8727256
doctormatterhorn <dmf030@gmail.com>
2026-08-07 21:33:05 -0300

    created .env and configured setting for deploying

--- config/settings.py
+++ config/settings.py
@@ -10,22 +10,34 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bf6@j96@^nf6rbh+9ur*c&k+_(u!tae#1rjdh5#2l-(6(b@h%a'
+# SECRET_KEY = 'django-insecure-bf6@j96@^nf6rbh+9ur*c&k+_(u!tae#1rjdh5#2l-(6(b@h%a'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.getenv("SITE_HOST", "127.0.0.1"),
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    f"https://{os.getenv('SITE_HOST', '127.0.0.1')}",
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -76,9 +88,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+
+        # Helpful for a small multi-user SQLite site.
+        "OPTIONS": {
+            "timeout": 20,
+        },
     }
 }
 
@@ -117,8 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
