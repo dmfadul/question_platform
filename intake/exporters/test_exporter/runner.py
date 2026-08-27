3da019b65f5a0fcf8490d7acf136802205b83121
doctormatterhorn <dmf030@gmail.com>
2026-08-27 09:33:51 -0300

    prepared runner.py to gen all tests at once

--- intake/exporters/test_exporter/runner.py
+++ intake/exporters/test_exporter/runner.py
@@ -1,7 +1,18 @@
 from .gen_test import generate_test
 
-TEST_TYPES = ["A", "B", "SEGUNDA_CHAMADA-A", "SEGUNDA_CHAMADA-B"]
+TEST_TYPES = ["A", "B"]
 CAREERS = ["APJ", "PAP", "DEL"]
+INCLUDE_SECOND_CHANCE = True
 
 def main():
+    for test_type in TEST_TYPES:
+        for career in CAREERS:
+            print(f"Generating test {test_type} for career {career}...")
+            # generate_test(test_type, career)
+
+        if INCLUDE_SECOND_CHANCE:
+            for career in CAREERS:
+                print(f"Generating second chance test SEGUNDA CHAMADA - {test_type} for career {career}...")
+                # generate_test(test_type, career, invert_question_order=True)
+
     generate_test("A", "APJ")
